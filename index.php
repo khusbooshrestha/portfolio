4c9745a24eb86ca3e9746d2a2338ee12ed93e03b
khusbooshrestha123@gmail.com <khusbooshrestha123@gmail.com>
2020-01-05 08:46:18 +0545

    changes

---
+++ index.php
@@ -0,0 +1,170 @@
+<?php include 'header.php' ?>
+  
+    
+      <section class="home-slider owl-carousel js-fullheight">
+        <div class="slider-item js-fullheight">
+          <div class="overlay"></div>
+          <div class="container">
+            <div class="row slider-text align-items-center text-center justify-content-center" data-scrollax-parent="true">
+              <div class="col-md-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                <p><a href="#" class="scroll">Hello! I'm</a></p>
+                <h1 class="mb-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Khusboo Shrestha</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="slider-item js-fullheight">
+          <div class="overlay"></div>
+          <div class="container">
+            <div class="row slider-text align-items-center text-center justify-content-center" data-scrollax-parent="true">
+              <div class="col-md-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                <p><a href="#" class="scroll">I'm from Kathmandu,Nepal.</a></p>
+                <h1 class="mb-3" data-scrollax="properties: {translateY: '30%', opacity: 1.6}">A Web Designer</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- END slider -->
+
+      <section class="ftco-section about-section">
+        <div class="container">
+          <div class="row d-flex" data-scrollax-parent="true">
+            <div class="col-md-4 author-img" style="background-image: url(images/khusi.jpg);" data-scrollax=" properties: { translateY: '-70%'}"></div>
+            <div class="col-md-2"></div>
+            <div class="col-md-6 wrap ftco-animate">
+              <div class="about-desc">
+                <h1 class="bold-text">About</h1>
+                <div class="p-5">
+                  <h2 class="mb-5">Hi! I'm Khusboo</h2>
+                  <p>I am a ambivert as a person and web-designer by profession.</p>
+                   <a href="cv.pdf" download>Click to view my resume</a>
+                  <ul class="ftco-footer-social list-unstyled mt-4">
+                    <li><a href="cv.pdf" download><span class="icon-download"></span></a></li>
+
+                    <li><a href="https://www.facebook.com/nisanishrestha" target="_blank"><span class="icon-facebook"></span></a></li>
+                    <li><a href="#"><span class="icon-instagram" target="_blank"></span></a></li>
+                    <li><a href="tel: +977-9848496316" target="_blank"><span class="icon-phone"></span></a></li>
+                    <li><a href="mailto:khusbooshrestha123@gmail.com target="_blank"><span class="icon-envelope"></span></a></li>
+
+
+                  </ul>
+                  <h5>Contact me here!</h5>
+                  <p>Email: <a href="mailto:khusbooshrestha123@gmail.com">khusbooshrestha123@gmail.com</a></p>
+                  <p>Phone: <a href="tel: +977-9848496316">+977-9848496316</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section class="ftco-section">
+        <div class="container">
+          <div class="row justify-content-center mb-5 pb-5">
+            <div class="col-md-7 text-center heading-section ftco-animate">
+              <span>Portfolio</span>
+              <h2>Checkout a few of my works</h2>
+            </div>
+          </div>
+          <div class="row no-gutters">
+          <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+            <a href="https://nepaltrekkingfo.com/" class="pic image d-flex justify-content-center align-items-center" style="background-image: url('images/work-1.png'); " data-scrollax=" properties: { translateY: '-30%'}">
+              <div class="icon d-flex text-center justify-content-center align-items-center">
+                <span class="icon-search"></span>
+              </div>
+            </a>
+            <div class="text">
+              <h4 class="subheading">Travel site</h4>
+              <h2 class="heading"><a href="https://nepaltrekkingfo.com/">Nepal trekinfo</a></h2>
+              <p>It is a travel site which needed a lot of effort as our client were very specific about their requirements.</p>
+              <p><a href="#">View Project</a></p>
+            </div>
+          </div>
+          <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+            <a href="http://theagrobazar.com/" class="pic image order-2 d-flex justify-content-center align-items-center" style="background-image: url('images/work-2.png'); " data-scrollax=" properties: { translateY: '-30%'}">
+              <div class="icon d-flex text-center justify-content-center align-items-center">
+                <span class="icon-search"></span>
+              </div>
+            </a>
+            <div class="text order-1">
+              <h4 class="subheading">Ecommerce</h4>
+              <h2 class="heading"><a href="#">Agrobazar</a></h2>
+              <p>It is a ecommerce site which encourages the buying of organic grocery items.</p>
+              <p><a href="http://theagrobazar.com/">View Project</a></p>
+            </div>
+          </div>
+          <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+            <a href="http://ultrabytegroup.com/resort/" class="pic image d-flex justify-content-center align-items-center" style="background-image: url('images/work-3.png'); " data-scrollax=" properties: { translateY: '-30%'}">
+              <div class="icon d-flex text-center justify-content-center align-items-center">
+                <span class="icon-search"></span>
+              </div>
+            </a>
+            <div class="text">
+              <h4 class="subheading">Resort</h4>
+              <h2 class="heading"><a href="http://ultrabytegroup.com/resort/">The river view resort</a></h2>
+              <p>This site is not yet live but in process and is one of favourite because of the slick and simple design it has.</p>
+              <p><a href="#">View Project</a></p>
+            </div>
+          </div>
+          <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+            <a href="https://royalmuskwine.com.au/" class="pic image order-2 d-flex justify-content-center align-items-center" style="background-image: url('images/work-4.png'); " data-scrollax=" properties: { translateY: '-30%'}">
+              <div class="icon d-flex text-center justify-content-center align-items-center">
+                <span class="icon-search"></span>
+              </div>
+            </a>
+            <div class="text order-1">
+              <h4 class="subheading">Ecommerce</h4>
+              <h2 class="heading"><a href="https://royalmuskwine.com.au/">Royal musk</a></h2>
+              <p>It is online platform for wine company based in Australia which mainly has 4 kind of wine.</p>
+              <p><a href="#">View Project</a></p>
+            </div>
+          </div>
+        </div>
+        </div>
+      </section>
+
+      <section class="ftco-section ftco-counter" id="section-counter">
+        <div class="container">
+          <div class="row justify-content-center mb-5 pb-5">
+            <div class="col-md-7 text-center heading-section ftco-animate">
+              <span>Portfolio</span>
+              <h2>I love to share my achievements</h2>
+            </div>
+          </div>
+          <div class="row d-flex justify-content-start">
+            <div class="col-md-5 col-sm-5 counter-wrap ftco-animate">
+              <div class="block-18">
+                <div class="text">
+                  <span class="ftco-label">Clients</span>
+                  <strong class="number" data-number="5">0</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row d-flex justify-content-center">
+            <div class="col-md-5 col-sm-5 counter-wrap ftco-animate">
+              <div class="block-18">
+                <div class="text">
+                  <span class="ftco-label">Project done</span>
+                  <strong class="number" data-number="26">0</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row d-flex justify-content-end">
+            <div class="col-md-5 counter-wrap ftco-animate">
+              <div class="block-18">
+                <div class="text">
+                  <span class="ftco-label">Cups of coffee</span>
+                  <strong class="number" data-number="100">0</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+<?php include 'footer.php' ?>
